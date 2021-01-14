6c2e613500f30a46f610a4eca4047d7013caa87f
Meran <meranzakelijk@gmail.com>
2021-01-14 01:21:51 +0100

    some user/places changes

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -22,7 +22,7 @@ const DUMMY_PLACES = [
     title: 'Empire State Building',
     description: 'One of the most famous sky scrapers in the world!',
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
+      'https://media.tacdn.com/media/attractions-splice-spp-674x446/06/71/33/e6.jpg',
     address: '20 W 34th St, New York, NY 10001',
     location: {
       lat: 40.7484405,
