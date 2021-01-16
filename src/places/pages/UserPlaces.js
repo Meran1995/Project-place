b0ae2d6aa2093e754ae6de4b2825c0ae9709da4e
Meran <meranzakelijk@gmail.com>
2021-01-16 19:35:28 +0100

    Modified my  Dummy user data (img, lan/lng, address, title ect

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -19,14 +19,14 @@ const DUMMY_PLACES = [
 	},
 	{
 		id: "p2",
-		title: "Empire State Building",
-		description: "One of the most famous sky scrapers in the world!",
+		title: "France - Eiffel Tower",
+		description: "One of the most famous building in Paris",
 		imageUrl:
-			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-		address: "20 W 34th St, New York, NY 10001",
+			"https://demodernenomaden.nl/wp-content/uploads/2018/11/wonen-en-werken-in-parijs-e1541539684522.jpg",
+		address: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris",
 		location: {
-			lat: 40.7484405,
-			lng: -73.9878584,
+			lat: 48.858093,
+			lng: 2.294694,
 		},
 		creator: "u2",
 	},
