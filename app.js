61537c6443d4df7fbcf812ad39ea50f5b0a1fd1d
Meran <meranzakelijk@gmail.com>
2021-01-24 00:05:30 +0100

    Adding Place-Specific Routes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const placesRoutes = require("./routes/places-routes");
 const app = express();
 
 // routing
-// placesRoutes is a middleware now and is actually router in the places-routes.js
-app.use(placesRoutes);
+// placesRoutes is a middleware
+app.use("/api/places", placesRoutes);
 
 app.listen(5000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,10 +2,41 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/",(req,res,next) => {
-	console.log("GET Request in Places");
-	res.json({message: "It Works!"});
-});
+const DUMMY_PLACES = [
+	{
+		id: "p1",
+		title: "Empire State Building",
+		description: "One of the most famous sky scrapers in the world!",
+		imageUrl:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+		address: "20 W 34th St, New York, NY 10001",
+		location: {
+			lat: 40.7484405,
+			lng: -73.9878584,
+		},
+		creator: "u1",
+	},
+	{
+		id: "p2",
+		title: "France - Eiffel Tower",
+		description: "One of the most famous building in Paris",
+		imageUrl:
+			"https://demodernenomaden.nl/wp-content/uploads/2018/11/wonen-en-werken-in-parijs-e1541539684522.jpg",
+		address: "Champ de Mars, 5 Avenue Anatole France, 75007 Paris",
+		location: {
+			lat: 48.858093,
+			lng: 2.294694,
+		},
+		creator: "u2",
+	},
+];
 
+router.get("/:pid", (req, res, next) => {
+	const placeId = req.params.pid;
+	const place = DUMMY_PLACES.find((p) => {
+		return p.id === placeId;
+	}); // { pid: "p1"}
+	res.json({ place }); // => {place } => { place: place }
+});
 
 module.exports = router;
