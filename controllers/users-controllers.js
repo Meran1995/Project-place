fa16080c5f877beedd5786344677fee087a49259
Meran <meranzakelijk@gmail.com>
2021-02-03 16:51:18 +0100

    Using the User model for Signup

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,8 +1,8 @@
 const uuid = require("uuid");
 const { validationResult } = require("express-validator");
 
-
 const HttpError = require("../models/http-error");
+const User = require("../models/user");
 
 const DUMMY_USERS = [
 	{
@@ -17,29 +17,50 @@ const getUsers = (req, res, next) => {
 	res.json({ users: DUMMY_USERS });
 };
 
-const signUp = (req, res, next) => {
+const signUp = async (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
 		throw new HttpError("invalid inputs passed, please check your data", 422);
 	}
-	
+
 	const { name, email, password } = req.body;
 
-	const hasUser = DUMMY_USERS.find(u => u.email === email);
-	if (hasUser) {
-		throw new HttpError ("Could not create user, email already exists.", 422);
+	let existingUser;
+	try {
+		existingUser = await User.findOne({ email: email });
+	} catch (err) {
+		const error = new HttpError(
+			"Signing up failed, please try again later",
+			500
+		);
+		return next(error);
 	}
 
-	const createdUser = {
-		id: uuid(),
+	if (existingUser) {
+		const error = new HttpError(
+			"User exist already, please login instead",
+			422
+		);
+		return next(error);
+	}
+
+	const createdUser = new User({
 		name,
 		email,
 		password,
-	};
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+		places,
+	});
 
-	DUMMY_USERS.push(createdUser);
+	try {
+		await createdPlace.save();
+	} catch (err) {
+		const error = new HttpError("Signing up failed, please try again", 500);
+		return next(error);
+	}
 
-	res.status(201).json({ user: createdUser });
+	res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
 const login = (req, res, next) => {
