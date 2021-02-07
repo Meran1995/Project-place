7577b2501a17216dc3f960292a73faff718bb613
Meran <meranzakelijk@gmail.com>
2021-02-07 19:20:36 +0100

    add CORS security for the connections with my front-end/back-end

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-
 const placesRoutes = require("./routes/places-routes");
 const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
@@ -10,6 +9,16 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+	next();
+});
 
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
@@ -25,7 +34,7 @@ app.use((error, req, res, next) => {
 	}
 	res.status(error.code || 500);
 	res.json({ message: error.message || "An unkown error occurred" });
-}); 
+});
 
 mongoose
 	.connect(
