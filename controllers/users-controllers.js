5e01ff462ca33cd6ae62d05a58ea7ae349340e2e
Meran <meranzakelijk@gmail.com>
2021-02-12 23:28:36 +0100

    Filtering files (Image only)

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -89,7 +89,10 @@ const login = async (req, res, next) => {
 		return next(error);
 	}
 
-	res.json({ message: "Logged in!", user: existingUser.toObject({getters: true}) });
+	res.json({
+		message: "Logged in!",
+		user: existingUser.toObject({ getters: true }),
+	});
 };
 
 exports.getUsers = getUsers;
